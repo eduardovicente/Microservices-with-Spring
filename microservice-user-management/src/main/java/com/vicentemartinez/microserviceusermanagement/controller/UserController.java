87e8f25e876b93e9a404683d542d692a4afb23b4
eduardovicente <eduardo.vicente@outlook.es>
2019-10-19 01:08:55 -0500

    Adding discovery methods for eureka server

--- microservice-user-management/src/main/java/com/vicentemartinez/microserviceusermanagement/controller/UserController.java
+++ microservice-user-management/src/main/java/com/vicentemartinez/microserviceusermanagement/controller/UserController.java
@@ -6,6 +6,9 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,6 +25,30 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
+	@Autowired
+	private DiscoveryClient discoveryClient;
+	
+	@Autowired
+	private Environment env;
+	
+	@Value("${spring.application.name}")
+	private String serviceId;
+	
+	@GetMapping("service/port")
+	public String getPort() {
+		return "Service port number: " + env.getProperty("local.server.port");
+	}
+	
+	@GetMapping("service/instances")
+	public ResponseEntity<?> getInstances() {
+		return new ResponseEntity<>(discoveryClient.getInstances(serviceId), HttpStatus.OK);
+	}
+
+	@GetMapping("service/services")
+	public ResponseEntity<?> getServices() {
+		return new ResponseEntity<>(discoveryClient.getServices(), HttpStatus.OK);
+	}
+
 	@PostMapping("/service/registration")
 	public ResponseEntity<?> saveUser(@RequestBody User user) {
 		if (userService.findByUserName(user.getName()) != null) {
@@ -35,18 +62,18 @@ public class UserController {
 	public ResponseEntity<?> getUser(HttpServletRequest request) {
 		Principal principal = request.getUserPrincipal();
 		if (principal == null || principal.getName() == null) {
-			return new ResponseEntity<>(HttpStatus.OK); 
+			return new ResponseEntity<>(HttpStatus.OK);
 		}
 		return ResponseEntity.ok(userService.findByUserName(principal.getName()));
 	}
-	
+
 	@PostMapping("/service/names")
-	public ResponseEntity<?> getNamesOfUsers(@RequestBody List<Long> idList){
+	public ResponseEntity<?> getNamesOfUsers(@RequestBody List<Long> idList) {
 		return ResponseEntity.ok(userService.findUser(idList));
 	}
-	
+
 	@GetMapping("/service/test")
-	public ResponseEntity<?> test(){
+	public ResponseEntity<?> test() {
 		return ResponseEntity.ok("It is working");
 	}
 }
