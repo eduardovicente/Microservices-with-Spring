65752ecf4bdbb1e9695387e52ddf7386348fe247
Eduardo Vicente <eduardo.vicente@outlook.es>
2019-10-23 13:04:36 -0500

    adding login component view and controller

--- course-enrollment-client/src/app/components/login/login.component.ts
+++ course-enrollment-client/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from "../../services/auth.service";
+import { User } from "../../model/user";
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: User = new User();
+  errorMessage: string;
 
-  constructor() { }
+  constructor(private authService : AuthService, private router : Router) { }
 
   ngOnInit() {
+  }
+
+  login(){
+    this.authService.logIn(this.user)
+      .subscribe(
+        data => {
+          this.router.navigate(['/profile']);
+        }, 
+        error => {
+          this.errorMessage = "userename or password is incorrect";
+        }
+      );
   }
 
 }
