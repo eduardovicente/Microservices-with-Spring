2f2c6abba4f18325a5dda65d53bfa6396dd0819c
Eduardo Vicente <eduardo.vicente@outlook.es>
2019-10-23 18:06:18 -0500

    starting with course functionality

--- course-enrollment-client/src/app/components/course-list/course-list.component.ts
+++ course-enrollment-client/src/app/components/course-list/course-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseService } from '../../services/course.service';
+import { Course } from "./../../model/Course";
+import { Transaction } from "./../../model/Transaction";
+import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-course-list',
@@ -7,7 +12,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private courseService: CourseService, private router: Router) { }
 
   ngOnInit() {
   }
--- course-enrollment-client/src/app/services/course.service.ts
+++ course-enrollment-client/src/app/services/course.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Course } from "./../model/Course";
+import { Transaction } from "./../model/Transaction";
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+let API_URL = 'http://localhost:8765/api/course/service/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  enroll(transaction: Transaction): Observable<any> {
+    return this.http.post(API_URL + "enroll", JSON.stringify(transaction),
+      {
+        headers: {
+          "Content-Type":"application/json; charset-UTF-8"
+        }
+      }
+    );
+  }
+
+  findAllCourses(): Observable<any> {
+    return this.http.post(API_URL,
+      {
+        headers: {
+          "Content-Type": "application/json; charset-UTF-8"
+        }
+      }
+    );
+  }
 }
