5215626dbc6a5522e4609be3167da1e71be52db7
eduardovicente <eduardo.vicente@outlook.es>
2019-10-22 22:43:47 -0500

    adding services and components

--- course-enrollment-client/src/app/app.module.ts
+++ course-enrollment-client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CourseComponent } from './components/course/course.component';
+import { CourseListComponent } from './components/course-list/course-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent,
+    CourseComponent,
+    CourseListComponent
   ],
   imports: [
     BrowserModule,
